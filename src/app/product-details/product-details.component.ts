de6125cc0fd92744d729d7ffcdcdee2215728fc1
Kirill Guydo <KirillGuydo@coherentsolutions.com>
2022-09-05 16:21:06 +0300

    Корзина.

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,33 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
-import { Product, products } from '../products';
-
-import { CartService } from '../cart.service';
+import { Product, products } from "../products";
+import { CartService } from "../cart.service";
 
 @Component({
-    selector: 'app-product-details',
-    templateUrl: './product-details.component.html',
-    styleUrls: ['./product-details.component.css'],
-
-
-
-  })
-export class 
-ProductDetailsComponent implements OnInit {
-    product:
-     Product | undefined;
+    selector: "app-product-details",
+    templateUrl: "./product-details.component.html",
+    styleUrls: ["./product-details.component.css"],
+})
+export class ProductDetailsComponent implements OnInit {
+    product: Product | undefined;
 
     constructor(
         private route: ActivatedRoute,
-
         private cartService: CartService
     ) {}
 
     ngOnInit(): void {
         const paramMap = this.route.snapshot.paramMap;
-        const productId = Number(paramMap.get('productId'));
+        const productId = Number(paramMap.get("productId"));
 
         this.product = products.find((p) => p.id === productId);
+    }
+
+    addToCart(product: Product) {
+        this.cartService.addToCart(product);
+        window.alert("Your product has been added to the cart.");
     }
 }
